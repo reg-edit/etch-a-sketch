f05aa95962066af1d79655747b857b52cec5c173
Reg-Edit <redwardsbusiness@gmail.com>
2023-02-06 15:06:03 +0000

    Add ability to change pixels and a basic pendown function

---
+++ script.js
@@ -0,0 +1,53 @@
+let sizeInput = document.querySelector("#sizeInput")
+let sizeSubmit = document.querySelector(".sizeSubmit")
+let sketchbox = document.querySelector(".sketchbox")
+let buttons = document.querySelectorAll("button")
+let squares = document.querySelectorAll(".s")
+
+
+
+
+function createDivs(n) {
+    for (let i = 0; i < n; i++) {
+        let row = document.createElement("div")
+        row.setAttribute("class", "row")
+        sketchbox.appendChild(row)
+        for (let j = 0; j < n; j++) {
+            let square = document.createElement("div")
+            square.setAttribute("class", "s")
+            row.appendChild(square)
+        }
+    }  
+    squares = document.querySelectorAll(".s")
+}
+
+function deleteDivs(parentNode) {
+    while (parentNode.firstChild) {
+        parentNode.removeChild(parentNode.firstChild)
+    }
+}
+
+
+sketchbox.addEventListener("mouseover", () => {
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener("mouseover", () => {
+            squares[i].removeAttribute("class", "s")
+            squares[i].setAttribute("class", "pendown")
+        })
+    }
+})
+
+/* Change Size Button */
+sizeSubmit.addEventListener("click", () => {
+    if ((sizeInput.value >= 1) && (sizeInput.value <= 100)) {
+        deleteDivs(sketchbox)
+        createDivs(sizeInput.value)
+        
+    }
+    else {
+        alert("Please enter a valid number between 1 and 100")
+    }    
+})
+
+
+createDivs(64)
